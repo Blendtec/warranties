2c00d66ff7fd97baa2ce45cef7aad8b9ea817f47
Stone C. Lasley <stonelasley@gmail.com>
2018-06-11 12:50:51 -0600

    fix(environment.prod/stage): Fix endpoint
    remove api segement from endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiHost: 'https://transit.blendtec.com',
   s3: 'https://s3-us-west-1.amazonaws.com/data.blendtec.com',
   bucket: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/warranty-claim/',
-  warrantiesEndPoint: '/api/warranty',
+  warrantiesEndPoint: '/warranty',
   assets: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/warranty-claim/assets',
   captchaKey: '6LcWmzIUAAAAADoSNPMqAECfcdIl9Z8B4czc4MjP'
 };
